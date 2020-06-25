6f0b9946c8d074ea9ac2c662a6a4c9fb2538bf60
Tarek Noubli <tareknoubli@gmail.com>
2020-06-25 16:11:19 +0200

    First commit: all assets added and folders for MVC.

---
+++ QuizzByOC/Model/Game.swift
@@ -0,0 +1,58 @@
+//
+//  Game.swift
+//  QuizzByOC
+//
+//  Created by Tarek Noubli on 25/06/2020.
+//  Copyright © 2020 Tarek. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+    var score = 0
+
+    private var questions = [Question]()
+    private var currentIndex = 0
+
+    var state: State = .ongoing
+
+    enum State {
+        case ongoing, over
+    }
+
+    var currentQuestion: Question {
+        return questions[currentIndex]
+    }
+
+    func refresh() {
+        score = 0
+        currentIndex = 0
+        state = .over
+        QuestionManager.shared.get { (questions) in
+          self.questions = questions
+          self.state = .ongoing
+          let name = Notification.Name(rawValue: "QuestionsLoaded")
+          let notification = Notification(name: name)
+          NotificationCenter.default.post(notification)
+        }
+    }
+
+    func answerCurrentQuestion(with answer: Bool) {
+        if (currentQuestion.isCorrect && answer) || (!currentQuestion.isCorrect && !answer) {
+            score += 1
+        }
+        goToNextQuestion()
+    }
+
+    private func goToNextQuestion() {
+        if currentIndex < questions.count - 1 {
+            currentIndex += 1
+        } else {
+            finishGame()
+        }
+    }
+
+    private func finishGame() {
+        state = .over
+    }
+}
---
+++ QuizzByOC/Model/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  QuizzByOC
+//
+//  Created by Tarek Noubli on 25/06/2020.
+//  Copyright © 2020 Tarek. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    var title = ""
+    var isCorrect = false
+}
