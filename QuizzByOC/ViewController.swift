7af341acd709a7e0565f27c431459cea27019a1a
Tarek Noubli <tareknoubli@gmail.com>
2020-06-25 14:04:44 +0200

    Initial Commit

---
+++ QuizzByOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizzByOC
+//
+//  Created by Tarek Noubli on 25/06/2020.
+//  Copyright © 2020 Tarek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
